09477b1c68e70d19996550bafd6e0197e2162948
Goya <leweeky@gmail.com>
2023-10-23 14:50:32 +0200

    amélioration echo -n

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -5,24 +5,20 @@ int ft_echo(char **envp, t_cmd *cmd)
 	int i;
 	int	new_line;
 
-	// retirer la newline avec l'option -n 
 	i = 1;
-	new_line = 1;
+	new_line = 0;
 	if (ft_isequal("-n", cmd->args[i]))
-	{
-		new_line = 0;
 		i++;
-	}
+	else
+		new_line = 1;
 	while (cmd->args[i])
 	{
-		if (new_line == 1 && i > 1)
+		if (new_line && i > 1 || !new_line && i > 2)
 			ft_putchar_fd(' ', 1);
 		ft_printf(cmd->args[i]);
-		if (new_line == 0)
-			ft_putchar_fd(' ', 1);
 		i++;
 	}
-	if (new_line == 1)
+	if (new_line)
 		ft_putendl_fd("", 1);
 	if (cmd && envp)
     	return (1);
