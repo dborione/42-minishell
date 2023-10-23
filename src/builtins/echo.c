adf0438bc0f5e551a7e0755ba788468399631e50
dborione <dborione@student.s19.be>
2023-10-23 14:47:00 +0200

    echo ok

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,16 +3,27 @@
 int ft_echo(char **envp, t_cmd *cmd)
 {
 	int i;
+	int	new_line;
 
+	// retirer la newline avec l'option -n 
 	i = 1;
-	while (cmd->args[i])
+	new_line = 1;
+	if (ft_isequal("-n", cmd->args[i]))
 	{
-		if (i > 1)
-			ft_putchar_fd(' ', 1);
-		ft_printf(cmd->args[i]);
+		new_line = 0;
 		i++;
 	}
-	ft_putendl_fd("", 1);
+	while (cmd->args[i])
+	{
+		if (new_line == 1 && i > 1)
+			ft_putchar_fd(' ', 1);
+		ft_printf(cmd->args[i]);
+		if (new_line == 0)
+			ft_putchar_fd(' ', 1);
+		i++;
+	}
+	if (new_line == 1)
+		ft_putendl_fd("", 1);
 	if (cmd && envp)
     	return (1);
 	return (1);
