090d315b48b95ddd6f00bcf7c234daf4fd1af509
Goya <leweeky@gmail.com>
2023-10-24 02:14:40 +0200

    retrait fonctions inutiles

--- src/commands/execution.c
+++ src/commands/execution.c
@@ -6,7 +6,7 @@
 /*   By: rbarbiot <rbarbiot@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 20:48:27 by rbarbiot          #+#    #+#             */
-/*   Updated: 2023/10/24 02:06:56 by rbarbiot         ###   ########.fr       */
+/*   Updated: 2023/10/24 02:14:27 by rbarbiot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,35 +98,35 @@ void	ft_next_command(t_shell_data **shell_data, t_cmd *cmd)
 	exit(EXIT_FAILURE);
 }
 
-static
-void	ft_next_commands(t_shell_data **shell_data, t_cmd *cmds)
-{
-	pid_t	parent;
-	//int				status;
+// static
+// void	ft_next_commands(t_shell_data **shell_data, t_cmd *cmds)
+// {
+// 	pid_t	parent;
+// 	//int				status;
 
-	if (cmds->next)
-	{
-		parent = fork();
-		if (parent < 0)
-			perror("bash");
-		if (parent == 0)
-			ft_next_command(shell_data, cmds);
-		else {
-			// if (dup2((*shell_data)->pipe[0], STDIN_FILENO) == -1)
-			// 	return ;
-			// close((*shell_data)->pipe[1]);
-			// (*shell_data)->input_fd = -1;
-			//waitpid(-1, NULL, 0);
-			ft_next_commands(shell_data, cmds->next);
-		} 
-	} else if ((*shell_data)->outfile && (*shell_data)->output_fd > -1) {
-		if (dup2((*shell_data)->output_fd, STDOUT_FILENO) == -1)
-			return ;
-		close((*shell_data)->output_fd);
-		(*shell_data)->output_fd = -1;
-	}
-	ft_next_command(shell_data, cmds);
-}
+// 	if (cmds->next)
+// 	{
+// 		parent = fork();
+// 		if (parent < 0)
+// 			perror("bash");
+// 		if (parent == 0)
+// 			ft_next_command(shell_data, cmds);
+// 		else {
+// 			// if (dup2((*shell_data)->pipe[0], STDIN_FILENO) == -1)
+// 			// 	return ;
+// 			// close((*shell_data)->pipe[1]);
+// 			// (*shell_data)->input_fd = -1;
+// 			//waitpid(-1, NULL, 0);
+// 			ft_next_commands(shell_data, cmds->next);
+// 		} 
+// 	} else if ((*shell_data)->outfile && (*shell_data)->output_fd > -1) {
+// 		if (dup2((*shell_data)->output_fd, STDOUT_FILENO) == -1)
+// 			return ;
+// 		close((*shell_data)->output_fd);
+// 		(*shell_data)->output_fd = -1;
+// 	}
+// 	ft_next_command(shell_data, cmds);
+// }
 
 static
 void	ft_multi_execution(t_shell_data **shell_data, t_cmd *cmds)
