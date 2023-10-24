c569c18fb14759b99104d7e5733fd3db2146efb7
dborione <dborione@student.s19.be>
2023-10-24 16:05:34 +0200

    modif pour export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -7,16 +7,16 @@ int ft_export(char **envp, t_cmd *cmd)
 
     i = 1;
     j = 0;
-
-    // while (cmd->args[i])
-    // while (envp[j])
-    //     j++;
-    //ft_putstr_fd(&envp[j][i], 1);
-    // printf("fdsfs\n");
-    printf("%c\n", envp[0][0]);
-    return (1);
-    if (cmd)
+    while (envp[j])
+        j++;
+    while (cmd->args[i])
     {
-        
+        //envp[j] = malloc (sizeof(cmd->args));
+        envp[j] = strdup(cmd->args[i]);
+        j++;
+        i++;
     }
+    printf("%s\n", envp[j - 1]);
+
+    return (1);
 }
