1aeffc732a7aba7ea2d07fe265ac3cfb148e033d
dborione <dborione@student.s19.be>
2023-11-02 16:02:04 +0100

    rajout des lignes effacees par le merge

--- src/input/get_input.c
+++ src/input/get_input.c
@@ -14,12 +14,12 @@
 
 void ft_get_input(t_shell_data **shell_data)
 {
-	//char    *hist_file;
-	char 			*line;
-	t_lexer_tokens 	*lexer_list;
-	char			**input;
+	// char    *hist_file;
+	char *line;
+	t_lexer_tokens *lexer_list;
+	char **input;
 
-	//hist_file = ft_strjoin(ft_envp_get_value((*shell_data)->envp, "HOME"), "/.history");
+	// hist_file = ft_strjoin(ft_envp_get_value((*shell_data)->envp, "HOME"), "/.history");
 	using_history();
 	stifle_history(1000);
 	while (!(*shell_data)->exit)
@@ -32,10 +32,17 @@ void ft_get_input(t_shell_data **shell_data)
 			if (isatty(STDIN_FILENO))
 				ft_putstr_fd("exit\n", STDOUT_FILENO);
 			free(line);
-			break ;
+			break;
+		};
+		input = ft_split_args(line);
+		if (!input)
+		{
+			perror("bash");
+			free(line);
+			break;
 		};
 		ft_printf("---\n");
-		int	y = 0;
+		int y = 0;
 		while (input[y])
 		{
 			ft_printf("(%s)\n", input[y]);
@@ -43,7 +50,7 @@ void ft_get_input(t_shell_data **shell_data)
 		}
 		ft_printf("---\n");
 		add_history(line);
-		//ft_add_to_history_file(&hist_file, line);
+		// ft_add_to_history_file(&hist_file, line);
 		lexer_list = ft_parse_input(shell_data, line);
 		free(line);
 		if (!lexer_list)
