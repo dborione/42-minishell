912d6ad4f62e86709170b62db9f04157f5f48487
dborione <dborione@student.s19.be>
2023-11-07 15:14:01 +0100

    replace putstr with perror

--- src/errors/export_error.c
+++ src/errors/export_error.c
@@ -2,8 +2,8 @@
 
 int	ft_export_error(char *arg)
 {
-    ft_putstr_fd("bash: export: `", STDERR_FILENO);
-	ft_putstr_fd(arg, STDERR_FILENO);
-	ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+    ft_perror("bash: export: `");
+	ft_perror(arg);
+	ft_perror("': not a valid identifier\n");
     return (1);
 }
