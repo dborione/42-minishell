bfb812f9a5885729bfb22386c90735d9df66e65d
dborione <dborione@student.s19.be>
2023-11-07 15:00:36 +0100

    add quotes to export error msg

--- src/errors/export_error.c
+++ src/errors/export_error.c
@@ -2,8 +2,8 @@
 
 int	ft_export_error(char *arg)
 {
-    ft_putstr_fd("bash: export: ", STDERR_FILENO);
+    ft_putstr_fd("bash: export: `", STDERR_FILENO);
 	ft_putstr_fd(arg, STDERR_FILENO);
-	ft_putstr_fd(": not a valid identifier\n", STDERR_FILENO);
+	ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
     return (1);
 }
