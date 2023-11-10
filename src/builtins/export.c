f6de84f98e98d5c6ce1f2b6dafbc6210aaeb177f
dborione <dborione@student.s19.be>
2023-11-10 15:40:33 +0100

    prise en charge += pour export, env ok mais pas print_export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -28,18 +28,39 @@ static
 int ft_is_in_env(char **envp, char *arg)
 {
     int i;
+    int j;
     char *key_env;
     char *key_arg;
+    char *new_arg;
 
     i = -1;
+    j = -1;
+    new_arg = NULL;
+    key_arg = NULL;
+    while (arg[++j])
+    {
+        if (arg[j] == '+')
+        {
+            key_arg = malloc(sizeof(char) * (j + 1));
+            ft_strlcpy(key_arg, arg, j + 1);
+            break ;
+        }
+    }
     while (envp[++i])
     {
         if (ft_isequal(envp[i], arg))
             return (0);
         key_env = ft_envp_get_key(envp[i]);
-        key_arg = ft_envp_get_key(arg);
         if (ft_isequal(key_env, key_arg))
         {
+            if (arg[j] == '+')
+            {
+                new_arg = ft_strjoin(envp[i], &arg[j + 2]);
+                free(envp[i]);
+                envp[i] = new_arg;
+                free (key_arg);
+                return (0);
+            }
             free (envp[i]);
             envp[i]= NULL;
             return (1);
@@ -70,7 +91,11 @@ int ft_add_to_export_env(char **export_envp, char *arg)
 
     j = 0;
     while (export_envp[j])
+    {
+        if (ft_isequal(export_envp[j], arg))
+            return (0);
         j++;
+    }
     export_envp[j] = ft_strdup(arg);
     export_envp[j + 1] = NULL;
     return (1);
@@ -89,8 +114,11 @@ int ft_check_valid_char(char *arg)
         if (!ft_isalnum(arg[i]))
         {
             if (arg[i] == '=' || arg[i] == '_')
-                return (1);
-            return (0);
+            {
+                if (ft_isalnum(arg[i - 1]) || arg[i - 1] == '+')
+                    return (1);
+                return (0);
+            }
         }
         i++;
     }
@@ -115,8 +143,7 @@ int ft_export(char **envp, char **export_envp, t_cmd *cmd)
         }
         if (ft_is_in_env(envp, cmd->args[i]) && ft_is_in_env(export_envp, cmd->args[i]))
         {
-            ft_add_to_export_env(export_envp, cmd->args[i]);
-            if (!ft_add_to_env(envp, cmd->args[i]))
+            if (!ft_add_to_export_env(export_envp, cmd->args[i]) || !ft_add_to_env(envp, cmd->args[i]))
                 i++;
         }
         i++;
