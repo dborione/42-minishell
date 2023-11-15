a898c22f8c73343867b924c5e2b34091ada47652
Goya <leweeky@gmail.com>
2023-11-15 13:44:50 +0100

    ajout des sources_fd / targets_fd pour les < >

--- src/start/init_all.c
+++ src/start/init_all.c
@@ -6,14 +6,11 @@
 /*   By: rbarbiot <rbarbiot@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 01:18:21 by rbarbiot          #+#    #+#             */
-/*   Updated: 2023/11/15 12:27:50 by rbarbiot         ###   ########.fr       */
+/*   Updated: 2023/11/15 13:44:47 by rbarbiot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
-
-static
-void	ft_init_fds()
 
 int		ft_init_all(t_shell_data **shell_data, char *shell_path, char *envp[])
 {
